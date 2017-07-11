3d9a0d704c7be7528e1fd696ae3b313796f7b93e
floonone <floonone@users.noreply.github.com>
2017-07-11 12:20:14 +0200

    Update get_image script for new camera_server

--- uvispace/uvisensor/resources/get_image.py
+++ uvispace/uvisensor/resources/get_image.py
@@ -7,24 +7,25 @@ def main():
     s = socket.socket(family=socket.AF_INET, type=socket.SOCK_STREAM)
     address = ("172.19.5.213", 36000)
     s.connect(address)
-    s.send("get_color\n")
+    s.send("capture_frame\n")
     try:
-        message = recv_data(s, 307200*3)
+        message = recv_data(s, 480 * 640 * 4)
     except socket.timeout:
         print("timeout")
     else:
-        shape = (480, 640, 3)
-        image = pylab.fromstring(message, dtype=pylab.uint8).reshape(shape)
-        misc.imsave("image.png", image)
+        mat = pylab.fromstring(message, dtype=pylab.uint8)
+        mat = mat.reshape((480, 640, 4))
+        arr = mat[:, :, 0:3]
+        misc.imsave("image.png", arr)
 
 
-def recv_data(socket, size):
+def recv_data(sck, size):
     recv_bytes = 0
     packages = []
     # Do not stop reading new packages until target 'size' is reached.
     while recv_bytes < size:
         try:
-            received_package = socket.recv(size)
+            received_package = sck.recv(size)
         except socket.timeout:
             break
         recv_bytes += len(received_package)
