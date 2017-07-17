510cbc90621162ad0c9400a12125cb2f1c17ab9e
Jose Lamas <joslamasvarela@gmail.com>
2017-07-17 10:28:17 +0200

    Fix bugs
    Fixed some bugs, and revised speedstudy module.

--- uvispace/uvirobot/speedstudy.py
+++ uvispace/uvirobot/speedstudy.py
@@ -1,17 +1,28 @@
 #!/usr/bin/env python
-"""
-Auxiliary program to move the car at speeds between 0 and 255 (binary).
+"""Auxiliary program to move the UGV at constant speed.
+
+This module allows the UGV to move at a constant speed for a given time.
+These values ​​are required to the user.
 """
 # Standard libraries
-import sys
 import getopt
+import logging
 import numpy as np
+import sys
 import time
 # Local libraries
 from messenger import connect_and_check
 from robot import RobotController
 from speedtransform import PolySpeedSolver
 
+try:
+    # Logging setup.
+    import settings
+except ImportError:
+    # Exit program if the settings module can't be found.
+    sys.exit("Can't find settings module. Maybe environment variables are not"
+             "set. Run the environment .sh script at the project root folder.")
+logger = logging.getLogger('speedstudy')
 
 def main():
     # Main routine
@@ -31,7 +42,6 @@ def main():
             sys.exit()
         elif opt in ("-r", "--robotid"):
             robot_id = int(arg)
-
     # Create an instance of SerMesProtocol and check connection to port.
     my_serial = connect_and_check(robot_id)
     my_robot = RobotController(robot_id)
@@ -40,21 +50,22 @@ def main():
                                          -0.4208, 22.21))
     right_solver = PolySpeedSolver(coefs=(141, 0.0902, -94.88, 0.0004565,
                                           0.6557, 22.59))
+    #TODO Check correct values
     linear = float(raw_input("Enter the linear speed value\n"))
     angular = float(raw_input("Enter the angular speed value\n"))
     operatingtime = float(raw_input("Enter the time to evaluate in seconds \n"))
     init_time = time.time()
     sp_left = int(left_solver.solve(linear, angular))
     sp_right = int(right_solver.solve(linear, angular))
-    print "I am sending (%d, %d)" % (sp_left, sp_right)
+    print "I am sending ({}, {})".format(sp_left, sp_right)
     while (time.time() - init_time) < operatingtime:
         my_serial.move([sp_right, sp_left])
+        print "I am sending ({}, {})".format(sp_left, sp_right)
     # When the desired time passes, the speed is zero
-    print "I am sending (%d, %d)" % (sp_left, sp_right)
     sp_right = 127
     sp_left = 127
     my_serial.move([sp_right, sp_left])
-    print "I am sending (%d, %d)" % (sp_left, sp_right)
+    print "I am sending ({}, {})".format(sp_left, sp_right)
 
 if __name__ == '__main__':
     main()
