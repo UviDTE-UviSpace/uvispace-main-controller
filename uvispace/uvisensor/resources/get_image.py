a82cffd0497f088b6b2bf55ac4675262ffbea73a
floonone <floonone@users.noreply.github.com>
2017-11-30 19:00:36 +0100

    Adapt get_image.py to new streaming server

--- uvispace/uvisensor/resources/get_image.py
+++ uvispace/uvisensor/resources/get_image.py
@@ -6,42 +6,19 @@ The localization node can be accessed through TCP/IP.
 """
 import numpy as np
 from scipy import misc
-import socket
+
+import zmq
 
 
 def main():
-    client = socket.socket(family=socket.AF_INET, type=socket.SOCK_STREAM)
-    address = ("172.19.5.213", 36000)
-    client.connect(address)
-    client.send("capture_frame\n")
-    try:
-        message = recv_data(client, 480*640*4)
-    except socket.timeout:
-        print("timeout")
-    else:
-        raw_array = np.fromstring(message, dtype=np.uint8)
-        raw_array = raw_array.reshape((480, 640, 4))
-        image_array = raw_array[:, :, 0:3]
-        misc.imsave("image.png", image_array)
+    receiver = zmq.Context.instance().socket(zmq.SUB)
+    receiver.connect("tcp://172.19.5.213:33000")
+    receiver.setsockopt_string(zmq.SUBSCRIBE, u"")
+    receiver.setsockopt(zmq.CONFLATE, True)
 
-    client.send("quit\n")
-
-
-def recv_data(sck, size):
-    """Read the specified number of packages from the input socket."""
-    recv_bytes = 0
-    packages = []
-    # Do not stop reading new packages until target 'size' is reached.
-    while recv_bytes < size:
-        try:
-            received_package = sck.recv(size)
-        except socket.timeout:
-            break
-        recv_bytes += len(received_package)
-        packages.append(received_package)
-    # Concatenate all the packages in a unique variable
-    data = ''.join(packages)
-    return data
+    message = receiver.recv()
+    image = np.fromstring(message, dtype=np.uint8).reshape((480, 640))
+    misc.imsave("image.png", image)
 
 if __name__ == '__main__':
     main()
