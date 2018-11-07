864f9b37bb5efe77e17c8c568fac32129053c58c
adpineiro <43777444+adpineiro@users.noreply.github.com>
2018-11-07 14:44:25 +0100

    Load IPs and image size from .cfg

--- uvispace/uvigui/image_procesing.py
+++ uvispace/uvigui/image_procesing.py
@@ -2,13 +2,15 @@
 import numpy as np
 import cv2
 import zmq
+import configparser
+from  PyQt5.QtGui import QImage, QPixmap
 import datetime
 import sys
 
 """
     Images are treated as arrays in cv2. Therefore, they can be formated with the numpy library.
     Images are stacked using the given path:
-    Image1         Image2
+    Image2         Image1
     
     Image3         Image4
     
@@ -17,56 +19,96 @@ import sys
 """
 
 
+def loadips():
+    """
+        :param cameras_IPs: array where the cameras IP are stored
+        Load the cameras IPs from the .cfg files
+        Uses the configparser lib to read .cfg files
+    """
+    cameras_IPs = []
+    for i in range(1, 5):
+        ipscam = configparser.ConfigParser()
+        rel_path = '../uvisensor/resources/config/video_sensor' + str(i) + '.cfg'
+        ipscam.read(rel_path)
+        print(ipscam.sections())
 
-def image_stack():
+        cameras_IPs.append(ipscam.get('VideoSensor', 'IP'))
+
+    print(cameras_IPs)
+    return cameras_IPs
+
+
+def load_image_size():
+    """
+
+    :param img_size: array where the iamge dimensions are stored
+    :return:
+
+    Load the image size from a .cfg file. Because all the cameras have the same size,
+    only reads one config file
+    """
+    size = configparser.ConfigParser()
+    rel_path = '../uvisensor/resources/config/video_sensor1.cfg'
+    size.read(rel_path)
+    img_size = [(size.getint('Camera', 'width')),
+                (size.getint('Camera', 'height'))]
+    return img_size
+
+
+
+def image_stack(cameras_ips, img_size, img_type):
+    """
+    Loads the four images and stacks them. Also writes the stackes image
+    :param widht:
+    :param height:
+    :param cameras_ips:
+    :param img_size:
+    :return:
+    """
     # load the four images
-    imagen1 = get_images(1, "BIN")
-    imagen2 = imagen1
-    imagen3 = imagen1
-    imagen4 = imagen1
-
-    #imagen2 = cv2.cvtColor(cv2.imread('imagen2.jpg'), cv2.COLOR_RGB2GRAY)
-    #imagen3 = cv2.cvtColor(cv2.imread('imagen3.jpg'), cv2.COLOR_RGB2GRAY)
-    #imagen4 = cv2.cvtColor(cv2.imread('imagen4.jpg'), cv2.COLOR_RGB2GRAY)
+    print(img_type)
+    image1 = get_images(cameras_ips[0], img_type, img_size)
+    image2 = get_images(cameras_ips[1], img_type, img_size)
+    image3 = get_images(cameras_ips[2], img_type, img_size)
+    image4 = get_images(cameras_ips[3], img_type, img_size)
+    #image1 = cv2.cvtColor(cv2.imread('imagen1.jpg'), cv2.COLOR_RGB2GRAY)
+    #image2 = cv2.cvtColor(cv2.imread('imagen2.jpg'), cv2.COLOR_RGB2GRAY)
+    #image3 = cv2.cvtColor(cv2.imread('imagen3.jpg'), cv2.COLOR_RGB2GRAY)
+    #image4 = cv2.cvtColor(cv2.imread('imagen4.jpg'), cv2.COLOR_RGB2GRAY)
     # Stack the matrix using concatenate
-    salida12 = np.concatenate((imagen1, imagen2), axis=1)  # apila las matrices 1 y 2
-    salida34 = np.concatenate((imagen3, imagen4), axis=1)  # apila las matrices 3 y 4
-    salida = np.concatenate((salida12, salida34), axis=0)  # resultante final
-
-    # resize the image to fit the label
-    salida_red = cv2.resize(salida, (640, 468))  # adjust the image to the label size
-    cv2.imwrite('salida.jpg', salida_red)  # guarda el array de imagenes como jpg
-
-    # muestra la imagen
+    output12 = np.concatenate((image2, image1), axis=1)  # apila las matrices 1 y 2
+    output34 = np.concatenate((image3, image4), axis=1)  # apila las matrices 3 y 4
+    output = np.concatenate((output12, output34), axis=0)  # resultante final
+    #resized = cv2.resize(output, (widht, height), interpolation=cv2.INTER_AREA)
+    cv2.imwrite('salida.jpg', output)  # saves the image as jpg
+    # show the image
     #   cv2.imshow('Imagen', salida_red)
     #  cv2.waitKey(0)
     #  cv2.destroyAllWindows()
 
 
-# image properties //TODO load from cfg file
-# copied directly from get-video-triangle
-IP_ADDRESS = "192.168.0.12"
-IMG_WIDTH = 640;
-IMG_HEIGHT = 468;
-IMG_TYPE_DEFAULT = "GRAY"
-
-
-def get_images(cam_num, img_type):
-    # copied directly from get-video-triangle, need to search for errors.
-    # Originally writen in Python 2...Now is Python 3.6, could be some errors
+def get_images(cam_ip, img_type, img_size):
+    """
+    Get video streaming from the cameras. Access throug TCP/IP
+    :param cam_ip: string with the IP
+    :param img_type: string with the image type. It could be "BIN" or "GRAY"
+    :param img_size: integer list with the height and the width of the image
+    :return:
+    """
     # Check the number of arguments passed to set resolution and img type
-
 
     receiver = zmq.Context.instance().socket(zmq.SUB)
     if img_type == "BIN":
-        receiver.connect("tcp://" + IP_ADDRESS + ":33000")
+        receiver.connect("tcp://" + cam_ip + ":33000")
     else:  # "GRAY" and "RGB"
-        receiver.connect("tcp://" + IP_ADDRESS + ":34000")
+        receiver.connect("tcp://" + cam_ip + ":34000")
     receiver.setsockopt_string(zmq.SUBSCRIBE, u"")
     receiver.setsockopt(zmq.CONFLATE, True)
 
-    message = receiver.recv()  # TODO error here, the programm freezes
-    image = np.fromstring(message, dtype=np.uint8).reshape((IMG_HEIGHT, IMG_WIDTH))
+    message = receiver.recv()
+    image = np.fromstring(message, dtype=np.uint8).reshape((img_size[1], img_size[0]))
 
     return image
+
+
 
