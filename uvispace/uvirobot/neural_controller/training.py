460242589876ab9a75cc3e0affd44f91f09360a8
Javier Grandio <javiergrandiogonzalez@gmail.com>
2019-03-21 20:10:20 +0100

    Only long training, new parameters

--- uvispace/uvirobot/neural_controller/training.py
+++ uvispace/uvirobot/neural_controller/training.py
@@ -4,7 +4,7 @@ from plot_ugv import PlotUgv
 from environment import UgvEnv
 import math
 from collections import deque
-import time
+import matplotlib.pyplot as plt
 
 
 
@@ -15,7 +15,7 @@ class Training:
         self.PERIOD= 1/30
         self.NUM_DIV_ACTION = 5
         self.INIT_TO_ZERO = True
-        self.EPISODES = 5000
+        self.EPISODES = 2000
         self.state_size = 2
         self.action_size = 5 * 5
 
@@ -82,8 +82,14 @@ class Training:
 
         print(reward_need)
         scores = deque(maxlen=50)
-        agent = Agent(self.state_size, self.action_size, gamma=0.995, epsilon=1, epsilon_min=0.01, epsilon_decay=0.999,
-                      learning_rate=0.005, batch_size=64, tau=0.01)
+        epi_reward_average=[]
+        #To plot velocity and distance to trayectory
+        epi_v_average=[]
+        epi_d_average=[]
+        v = deque(maxlen=50)
+        d = deque(maxlen=50)
+        agent = Agent(self.state_size, self.action_size, gamma=0.999, epsilon=1, epsilon_min=0.01, epsilon_decay=0.99,
+                      learning_rate=0.01, batch_size=128, tau=0.01)
         plot_ugv = PlotUgv(self.SPACE_X, self.SPACE_Y, x_trajectory, y_trajectory, self.PERIOD)
         env = UgvEnv(x_trajectory, y_trajectory, self.PERIOD,
                      self.NUM_DIV_ACTION, closed=False)
@@ -95,12 +101,16 @@ class Training:
             agent_state = agent.format_state(agent_state)
             done = False
             R = 0
+            epi_v=[]
+            epi_d=[]
             #if e%500==0:
             #    plot_ugv.reset(state)
 
             while not done:
                 action = agent.action(agent_state)
                 new_state, new_agent_state, reward, done = env.step(action)
+                epi_v.append(env.v_linear)
+                epi_d.append(np.sqrt(new_agent_state[0]**2))
                 #if e % 500 == 0:
                 #    plot_ugv.execute(state)
                 new_agent_state = agent.format_state(new_agent_state)
@@ -114,13 +124,149 @@ class Training:
                 agent.soft_update_target_network()
             agent.reduce_random()
             scores.append(R)
+            v.append(np.mean(epi_v))
+            d.append(np.mean(epi_d))
             mean_score = np.mean(scores)
+            epi_reward_average.append(np.mean(scores))
+            epi_v_average.append(np.mean(v))
+            epi_d_average.append(np.mean(d))
+
             print("episode: {}/{}, score: {}, e: {:.2}, mean_score: {}, final state :({},{})"
                   .format(e, self.EPISODES, R, agent.epsilon, mean_score, env.state[0], env.state[1]))
 
             if mean_score > reward_need:
                 agent.save_model(save_name)
                 break
+        fig, ax = plt.subplots()
+        fig.suptitle('Rewards Curve')
+        ax.plot(range(len(epi_reward_average)), epi_reward_average, label='Deep Q-Learning')
+        legend = ax.legend(loc='lower right', shadow=True, fontsize='x-large')
+        plt.xlabel("Episode")
+        plt.ylabel("Reward")
+        plt.show()
+
+        fig, ax = plt.subplots()
+        fig.suptitle('Mean Velocity')
+        ax.plot(range(len(epi_v_average)), epi_v_average, label='Deep Q-Learning')
+        legend = ax.legend(loc='lower right', shadow=True, fontsize='x-large')
+        plt.xlabel("Episode")
+        plt.ylabel("Velocity")
+        plt.show()
+
+        fig, ax = plt.subplots()
+        fig.suptitle('Mean Distance to Trajectory')
+        ax.plot(range(len(epi_d_average)), epi_d_average, label='Deep Q-Learning')
+        legend = ax.legend(loc='lower right', shadow=True, fontsize='x-large')
+        plt.xlabel("Episode")
+        plt.ylabel("Distance")
+        plt.show()
+
+
+
+    def trainclosedcircuit(self,load=False, load_name='emodel.h5', save_name='emodel.h5',reward_need=100):
+
+        x_trajectory = np.append(np.linspace(0.2, 0.2, 41),
+                                 np.cos(np.linspace(180 * math.pi / 180, 90 * math.pi / 180, 61)) * 0.1 + 0.3)
+        y_trajectory = np.append(np.linspace(0.2, 0.4, 41),
+                                 np.sin(np.linspace(180 * math.pi / 180, 90 * math.pi / 180, 61)) * 0.1 + 0.4)
+        x_trajectory = np.append(x_trajectory,
+                                 np.cos(np.linspace(270 * math.pi / 180, 360 * math.pi / 180, 81)) * 0.2 + 0.3)
+        y_trajectory = np.append(y_trajectory,
+                                 np.sin(np.linspace(270 * math.pi / 180, 360 * math.pi / 180, 81)) * 0.2 + 0.7)
+        x_trajectory = np.append(x_trajectory,
+                                 np.cos(np.linspace(180 * math.pi / 180, 0 * math.pi / 180, 141)) * 0.3 + 0.8)
+        y_trajectory = np.append(y_trajectory,
+                                 np.sin(np.linspace(180 * math.pi / 180, 0 * math.pi / 180, 141)) * 0.3 + 0.7)
+        x_trajectory = np.append(x_trajectory, np.linspace(1.1, 1.1, 81))
+        y_trajectory = np.append(y_trajectory, np.linspace(0.7, 0.3, 81))
+        x_trajectory = np.append(x_trajectory,
+                                 np.cos(np.linspace(0 * math.pi / 180, -90 * math.pi / 180, 81)) * 0.3 + 0.8)
+        y_trajectory = np.append(y_trajectory,
+                                 np.sin(np.linspace(0 * math.pi / 180, -90 * math.pi / 180, 81)) * 0.3 + 0.3)
+        x_trajectory = np.append(x_trajectory, np.linspace(0.8, 0.4, 81))
+        y_trajectory = np.append(y_trajectory, np.linspace(0, 0, 81))
+        x_trajectory = np.append(x_trajectory,
+                                 np.cos(np.linspace(270 * math.pi / 180, 180 * math.pi / 180, 81)) * 0.2 + 0.4)
+        y_trajectory = np.append(y_trajectory,
+                                 np.sin(np.linspace(270 * math.pi / 180, 180 * math.pi / 180, 81)) * 0.2 + 0.2)
+
+
+        scores = deque(maxlen=50)
+        epi_reward_average = []
+        # To plot velocity and distance to trayectory
+        epi_v_average = []
+        epi_d_average = []
+        v = deque(maxlen=50)
+        d = deque(maxlen=50)
+        agent = Agent(self.state_size, self.action_size, gamma=0.999, epsilon=1, epsilon_min=0.01, epsilon_decay=0.995,
+                      learning_rate=0.01, batch_size=128, tau=0.01)
+        plot_ugv = PlotUgv(self.SPACE_X, self.SPACE_Y, x_trajectory, y_trajectory, self.PERIOD)
+        env = UgvEnv(x_trajectory, y_trajectory, self.PERIOD,
+                     self.NUM_DIV_ACTION, closed=True)
+        if load:
+            agent.load_model(load_name)
+
+        for e in range(self.EPISODES):
+            state, agent_state = env.reset()
+            agent_state = agent.format_state(agent_state)
+            done = False
+            R = 0
+            epi_v = []
+            epi_d = []
+
+            while not done:
+                action = agent.action(agent_state)
+                new_state, new_agent_state, reward, done = env.step(action)
+                epi_v.append(env.v_linear)
+                epi_d.append(np.sqrt(new_agent_state[0]**2))
+                new_agent_state = agent.format_state(new_agent_state)
+                agent.remember(agent_state, action, reward, new_agent_state, done)
+
+                agent_state = new_agent_state
+                R += reward
+
+            if len(agent.memory) > agent.batch_size:
+                agent.replay()
+                agent.soft_update_target_network()
+            agent.reduce_random()
+            scores.append(R)
+            v.append(np.mean(epi_v))
+            d.append(np.mean(epi_d))
+            mean_score = np.mean(scores)
+            epi_reward_average.append(np.mean(scores))
+            epi_v_average.append(np.mean(v))
+            epi_d_average.append(np.mean(d))
+            print("episode: {}/{}, score: {}, e: {:.2}, mean_score: {}, final state :({},{})"
+                  .format(e, self.EPISODES, R, agent.epsilon, mean_score, env.state[0], env.state[1]))
+
+            if mean_score > reward_need:
+                agent.save_model(save_name)
+                break
+
+        fig, ax = plt.subplots()
+        fig.suptitle('Rewards Curve')
+        ax.plot(range(len(epi_reward_average)), epi_reward_average, label='Deep Q-Learning')
+        legend = ax.legend(loc='lower right', shadow=True, fontsize='x-large')
+        plt.xlabel("Episode")
+        plt.ylabel("Reward")
+        plt.show()
+
+        fig, ax = plt.subplots()
+        fig.suptitle('Mean Velocity')
+        ax.plot(range(len(epi_v_average)), epi_v_average, label='Deep Q-Learning')
+        legend = ax.legend(loc='lower right', shadow=True, fontsize='x-large')
+        plt.xlabel("Episode")
+        plt.ylabel("Velocity")
+        plt.show()
+
+        fig, ax = plt.subplots()
+        fig.suptitle('Mean Distance to Trajectory')
+        ax.plot(range(len(epi_d_average)), epi_d_average, label='Deep Q-Learning')
+        legend = ax.legend(loc='lower right', shadow=True, fontsize='x-large')
+        plt.xlabel("Episode")
+        plt.ylabel("Distance")
+        plt.show()
+
 
     def traincurve2(self, load=False, load_name='emodel.h5', save_name='emodel.h5'):
         x_trajectory = np.linspace(0.2, 0.2, 121)
@@ -319,93 +465,7 @@ class Training:
                 agent.save_model(save_name)
                 break
 
-    def trainclosedcircuit(self,load=False, load_name='emodel.h5', save_name='emodel.h5',reward_need=100):
 
-        x_trajectory = np.append(np.linspace(0.2, 0.2, 41),
-                                 np.cos(np.linspace(180 * math.pi / 180, 90 * math.pi / 180, 61)) * 0.1 + 0.3)
-        y_trajectory = np.append(np.linspace(0.2, 0.4, 41),
-                                 np.sin(np.linspace(180 * math.pi / 180, 90 * math.pi / 180, 61)) * 0.1 + 0.4)
-        x_trajectory = np.append(x_trajectory,
-                                 np.cos(np.linspace(270 * math.pi / 180, 360 * math.pi / 180, 81)) * 0.2 + 0.3)
-        y_trajectory = np.append(y_trajectory,
-                                 np.sin(np.linspace(270 * math.pi / 180, 360 * math.pi / 180, 81)) * 0.2 + 0.7)
-        x_trajectory = np.append(x_trajectory,
-                                 np.cos(np.linspace(180 * math.pi / 180, 0 * math.pi / 180, 141)) * 0.3 + 0.8)
-        y_trajectory = np.append(y_trajectory,
-                                 np.sin(np.linspace(180 * math.pi / 180, 0 * math.pi / 180, 141)) * 0.3 + 0.7)
-        x_trajectory = np.append(x_trajectory, np.linspace(1.1, 1.1, 81))
-        y_trajectory = np.append(y_trajectory, np.linspace(0.7, 0.3, 81))
-        x_trajectory = np.append(x_trajectory,
-                                 np.cos(np.linspace(0 * math.pi / 180, -90 * math.pi / 180, 81)) * 0.3 + 0.8)
-        y_trajectory = np.append(y_trajectory,
-                                 np.sin(np.linspace(0 * math.pi / 180, -90 * math.pi / 180, 81)) * 0.3 + 0.3)
-        x_trajectory = np.append(x_trajectory, np.linspace(0.8, 0.4, 81))
-        y_trajectory = np.append(y_trajectory, np.linspace(0, 0, 81))
-        x_trajectory = np.append(x_trajectory,
-                                 np.cos(np.linspace(270 * math.pi / 180, 180 * math.pi / 180, 81)) * 0.2 + 0.4)
-        y_trajectory = np.append(y_trajectory,
-                                 np.sin(np.linspace(270 * math.pi / 180, 180 * math.pi / 180, 81)) * 0.2 + 0.2)
-        #x_trajectory = np.append(np.linspace(0.2, 0.2, 41),
-        #                        np.cos(np.linspace(180 * math.pi / 180, 90 * math.pi / 180, 61)) * 0.1 + 0.3)
-        #y_trajectory = np.append(np.linspace(0.2, 0.4, 41),
-        #                        np.sin(np.linspace(180 * math.pi / 180, 90 * math.pi / 180, 61)) * 0.1 + 0.4)
-        #x_trajectory = np.append(x_trajectory,
-        #                        np.cos(np.linspace(270 * math.pi / 180, 360 * math.pi / 180, 81)) * 0.2 + 0.3)
-        #y_trajectory = np.append(y_trajectory,
-        #                        np.sin(np.linspace(270 * math.pi / 180, 360 * math.pi / 180, 81)) * 0.2 + 0.7)
-        #x_trajectory = np.append(x_trajectory,
-        #                        np.cos(np.linspace(180 * math.pi / 180, -90 * math.pi / 180, 141)) * 0.3 + 0.8)
-        #y_trajectory = np.append(y_trajectory,
-        #                        np.sin(np.linspace(180 * math.pi / 180, -90 * math.pi / 180, 141)) * 0.3 + 0.7)
-        #x_trajectory = np.append(x_trajectory,
-        #                        np.cos(np.linspace(90 * math.pi / 180, 180 * math.pi / 180, 61)) * 0.1 + 0.8)
-        #y_trajectory = np.append(y_trajectory,
-        #                        np.sin(np.linspace(90 * math.pi / 180, 180 * math.pi / 180, 61)) * 0.1 + 0.3)
-        #x_trajectory = np.append(x_trajectory,
-        #                        np.cos(np.linspace(360 * math.pi / 180, 270 * math.pi / 180, 61)) * 0.3 + 0.4)
-        #y_trajectory = np.append(y_trajectory,
-        #                        np.sin(np.linspace(360 * math.pi / 180, 270 * math.pi / 180, 61)) * 0.3 + 0.3)
-        #x_trajectory = np.append(x_trajectory,
-        #                        np.cos(np.linspace(270 * math.pi / 180, 180 * math.pi / 180, 81)) * 0.2 + 0.4)
-        #y_trajectory = np.append(y_trajectory,
-        #                         np.sin(np.linspace(270 * math.pi / 180, 180 * math.pi / 180, 81)) * 0.2 + 0.2)
-
-        scores = deque(maxlen=50)
-        agent = Agent(self.state_size, self.action_size, gamma=0.99, epsilon=0.3, epsilon_min=0.01, epsilon_decay=0.999,
-                      learning_rate=0.005, batch_size=64, tau=0.01)
-        plot_ugv = PlotUgv(self.SPACE_X, self.SPACE_Y, x_trajectory, y_trajectory, self.PERIOD)
-        env = UgvEnv(x_trajectory, y_trajectory, self.PERIOD,
-                     self.NUM_DIV_ACTION, closed=True)
-        if load:
-            agent.load_model(load_name)
-
-        for e in range(self.EPISODES):
-            state, agent_state = env.reset()
-            agent_state = agent.format_state(agent_state)
-            done = False
-            R = 0
-
-            while not done:
-                action = agent.action(agent_state)
-                new_state, new_agent_state, reward, done = env.step(action)
-                new_agent_state = agent.format_state(new_agent_state)
-                agent.remember(agent_state, action, reward, new_agent_state, done)
-
-                agent_state = new_agent_state
-                R += reward
-
-            if len(agent.memory) > agent.batch_size:
-                agent.replay()
-                agent.soft_update_target_network()
-            agent.reduce_random()
-            scores.append(R)
-            mean_score = np.mean(scores)
-            print("episode: {}/{}, score: {}, e: {:.2}, mean_score: {}, final state :({},{})"
-                  .format(e, self.EPISODES, R, agent.epsilon, mean_score, env.state[0], env.state[1]))
-
-            if mean_score > reward_need:
-                agent.save_model(save_name)
-                break
 
 
 
@@ -423,32 +483,50 @@ class Training:
                      self.NUM_DIV_ACTION, closed=closed)
         agent.load_model(load_name)
 
-        for e in range(5):
+        for e in range(10):
             state, agent_state = env.reset()
             agent_state = agent.format_state(agent_state)
             plot_ugv.reset(state)
             done = False
             R = 0
             v = deque()
+            d=deque()
             while not done:
                 action = np.argmax(agent.model.predict(agent_state))
                 new_state, new_agent_state, reward, done = env.step(action)
+                v.append(env.v_linear)
+                d.append(np.sqrt(env.distance**2))
                 new_agent_state = agent.format_state(new_agent_state)
                 plot_ugv.execute(new_state)
-                grados=new_agent_state[0][1]*180/math.pi
-                grados2=new_state[2]*180/math.pi
-                grados3=env.trajec_angle*180/math.pi
-                print(grados3,'    ',grados2,'    ', grados)
+                #grados=new_agent_state[0][1]*180/math.pi
+                #grados2=new_state[2]*180/math.pi
+                #grados3=env.trajec_angle*180/math.pi
+                #print(grados3,'    ',grados2,'    ', grados)
                 agent_state = new_agent_state
                 R += reward
-                v.append(env.v_linear)
+
             scores.append(R)
             mean_score = np.mean(scores)
             mean_v = np.mean(v)
-            print("episode: {}/{}, score: {}, laps: {:}, mean_score: {}, final state :({},{}), velocidad media: {}"
-                  .format(e, self.EPISODES, R, env.laps, mean_score, env.state[0], env.state[1], mean_v))
+            mean_d = np.mean(d)
+            print("episode: {}/{}, score: {}, laps: {:}, mean_score: {}, final state :({},{}), velocidad media: {}, Distancia media: {}"
+                  .format(e, self.EPISODES, R, env.laps, mean_score, env.state[0], env.state[1], mean_v, mean_d))
 
+            fig, ax = plt.subplots()
+            fig.suptitle('Velocity')
+            ax.plot(range(len(v)), v, label='Deep Q-Learning')
+            legend = ax.legend(loc='lower right', shadow=True, fontsize='x-large')
+            plt.xlabel("Step")
+            plt.ylabel("Velocity")
+            plt.show()
 
+            fig, ax = plt.subplots()
+            fig.suptitle('Distance to Trajectory')
+            ax.plot(range(len(d)), d, label='Deep Q-Learning')
+            legend = ax.legend(loc='lower right', shadow=True, fontsize='x-large')
+            plt.xlabel("Step")
+            plt.ylabel("Distance")
+            plt.show()
 
 if __name__ == "__main__":
     #tengo que quitarle la indentacion a todo
