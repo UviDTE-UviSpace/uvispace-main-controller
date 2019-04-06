dbc1e4f49af74676373667fa216a0d509a94ae64
robertofem <robertofem@gmail.com>
2019-04-06 14:23:00 +0200

    create the UviSpace object that permits to launch all UviSpace packages at once

--- uvispace/__main__.py
+++ uvispace/__main__.py
@@ -1,32 +1,8 @@
 """
 Invokes the main Uvispace package. It launches all uvispace sub packages.
-Each sub package goes in a different thread
+Each sub package goes in a different thread (except GUI that remains in this one)
 """
-
-from distutils.util import strtobool
-
-from uvispace.uvisensor.uvisensor import UviSensor
-from uvispace.uvisensor.common import ImgType
+from uvispace.uvispace import UviSpace
 
 if __name__ == "__main__":
-
-    # read configuration
-    configuration = configparser.ConfigParser()
-    conf_file = "uvispace/config.cfg"
-    configuration.read(conf_file)
-    gui_enabled = strtobool(configuration["Run"]["gui_enabled"])
-
-    # launch Uvisensor package
-    sensor = UviSensor(enable_img = True, enable_triang = True)
-    # starts stream (triangles and multi-images) in other thread
-    sensor.start_stream()
-
-    # Launch UviRobot
-    pass
-
-    # Launch UviNavigator
-    pass
-
-    # launch UviGui package
-    if gui_enabled:
-        pass
+    u = UviSpace().start()
---
+++ uvispace/uvispace.py
@@ -0,0 +1,46 @@
+from distutils.util import strtobool
+import threading
+import configparser
+
+from uvispace.uvisensor.uvisensor import UviSensor
+from uvispace.uvisensor.common import ImgType
+
+class UviSpace():
+    def __init__(self):
+        # read configuration
+        configuration = configparser.ConfigParser()
+        conf_file = "uvispace/config.cfg"
+        configuration.read(conf_file)
+        self.gui_enabled = strtobool(configuration["Run"]["gui_enabled"])
+
+        # create some threads to launch the different modules
+        self.thread_UviSensor = threading.Thread(target = self.launch_UviSensor)
+        self.thread_UviNavigator = threading.Thread(target = self.launch_UviNavigator)
+        self.thread_UviRobot = threading.Thread(target = self.launch_UviRobot)
+
+    def start(self):
+        # launch UviSensor in a new thread
+        self.thread_UviSensor.start()
+
+        # Launch UviRobot
+        pass
+
+        # Launch UviNavigator
+        pass
+
+        # launch UviGui package
+        if self.gui_enabled:
+            pass
+        else:
+            while(1):
+                pass
+
+    def launch_UviSensor(self):
+        sensor = UviSensor(enable_img = True, enable_triang = False)
+        sensor.start_stream()
+
+    def launch_UviNavigator():
+        pass
+
+    def launch_UviRobot(self):
+        pass
