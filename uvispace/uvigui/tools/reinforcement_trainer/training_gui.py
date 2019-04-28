4a4155edab9e107267291c7192dded56fa9dd1e8
Javier Grandio <javiergrandiogonzalez@gmail.com>
2019-04-28 21:54:01 +0200

    trajectories loaded from csv

--- uvispace/uvigui/tools/reinforcement_trainer/training_gui.py
+++ uvispace/uvigui/tools/reinforcement_trainer/training_gui.py
@@ -142,63 +142,28 @@ class MainWindow(QtWidgets.QMainWindow, reinforcement.Ui_fuzzy_window):
 
         self.ts = Testing()
         if self.rbDifferential.isChecked():
-            x_trajectory = np.append(np.linspace(0.2, 0.2, 41),
-                                     np.cos(np.linspace(180 * np.pi / 180, 90 * np.pi / 180, 61)) * 0.1 + 0.3)
-            y_trajectory = np.append(np.linspace(0.2, 0.4, 41),
-                                     np.sin(np.linspace(180 * np.pi / 180, 90 * np.pi / 180, 61)) * 0.1 + 0.4)
-            x_trajectory = np.append(x_trajectory,
-                                     np.cos(np.linspace(270 * np.pi / 180, 360 * np.pi / 180, 81)) * 0.2 + 0.3)
-            y_trajectory = np.append(y_trajectory,
-                                     np.sin(np.linspace(270 * np.pi / 180, 360 * np.pi / 180, 81)) * 0.2 + 0.7)
-            x_trajectory = np.append(x_trajectory,
-                                     np.cos(np.linspace(180 * np.pi / 180, -90 * np.pi / 180, 141)) * 0.3 + 0.8)
-            y_trajectory = np.append(y_trajectory,
-                                     np.sin(np.linspace(180 * np.pi / 180, -90 * np.pi / 180, 141)) * 0.3 + 0.7)
-            x_trajectory = np.append(x_trajectory,
-                                     np.cos(np.linspace(90 * np.pi / 180, 180 * np.pi / 180, 61)) * 0.1 + 0.8)
-            y_trajectory = np.append(y_trajectory,
-                                     np.sin(np.linspace(90 * np.pi / 180, 180 * np.pi / 180, 61)) * 0.1 + 0.3)
-            x_trajectory = np.append(x_trajectory,
-                                     np.cos(np.linspace(360 * np.pi / 180, 270 * np.pi / 180, 61)) * 0.3 + 0.4)
-            y_trajectory = np.append(y_trajectory,
-                                     np.sin(np.linspace(360 * np.pi / 180, 270 * np.pi / 180, 61)) * 0.3 + 0.3)
-            x_trajectory = np.append(x_trajectory,
-                                     np.cos(np.linspace(270 * np.pi / 180, 180 * np.pi / 180, 81)) * 0.2 + 0.4)
-            y_trajectory = np.append(y_trajectory,
-                                     np.sin(np.linspace(270 * np.pi / 180, 180 * np.pi / 180, 81)) * 0.2 + 0.2)
+
+            #Read csv file
+            coordinates = np.loadtxt(open("uvispace/uvigui/tools/reinforcement_trainer/resources/testing_differential.csv", "r"), delimiter=";")
+            x_trajectory=[]
+            y_trajectory=[]
+            for point in coordinates:
+                x_trajectory.append(point[0])
+                y_trajectory.append(point[1])
 
             self.ts.testing(load_name=self.hdf5_file_name, x_trajectory=x_trajectory, y_trajectory=y_trajectory, closed=False, differential_car=True)
 
         elif self.rbAckerman.isChecked():
-            x_trajectory = np.append(np.linspace(0.2, 0.2, 41),
-                                     np.cos(np.linspace(180 * math.pi / 180, 0.001 * math.pi / 180, 161)) * 0.8 + 1)
-            y_trajectory = np.append(np.linspace(0.2, 0.399, 41),
-                                     np.sin(np.linspace(180 * math.pi / 180, 0.001 * math.pi / 180, 161)) * 0.8 + 0.4)
-            x_trajectory = np.append(x_trajectory, np.linspace(1.8, 1.8, 181))
-            y_trajectory = np.append(y_trajectory, np.linspace(0.4, -0.699, 181))
-            x_trajectory = np.append(x_trajectory,
-                                     np.cos(np.linspace(360 * math.pi / 180, 270.001 * math.pi / 180, 81)) * 0.7 + 1.1)
-            y_trajectory = np.append(y_trajectory,
-                                     np.sin(np.linspace(360 * math.pi / 180, 270.001 * math.pi / 180, 81)) * 0.7 - 0.7)
-            x_trajectory = np.append(x_trajectory, np.linspace(1.1, -1.099, 41))
-            y_trajectory = np.append(y_trajectory, np.linspace(-1.4, -1.4, 41))
-            x_trajectory = np.append(x_trajectory,
-                                     np.cos(np.linspace(270 * math.pi / 180, 180.001 * math.pi / 180, 81)) * 0.8 - 1.1)
-            y_trajectory = np.append(y_trajectory,
-                                     np.sin(np.linspace(270 * math.pi / 180, 180.001 * math.pi / 180, 81)) * 0.8 - 0.6)
-            x_trajectory = np.append(x_trajectory, np.linspace(-1.9, -1.9, 161))
-            y_trajectory = np.append(y_trajectory, np.linspace(-0.6, 0.1999, 161))
-            x_trajectory = np.append(x_trajectory,
-                                     np.cos(
-                                         np.linspace(180 * math.pi / 180, 0.001 * math.pi / 180, 161)) * 0.525 - 1.375)
-            y_trajectory = np.append(y_trajectory,
-                                     np.sin(np.linspace(180 * math.pi / 180, 0.001 * math.pi / 180, 161)) * 0.525 + 0.2)
-            x_trajectory = np.append(x_trajectory,
-                                     np.cos(np.linspace(180 * math.pi / 180, 359.999 * math.pi / 180,
-                                                        191)) * 0.525 - 0.325)
-            y_trajectory = np.append(y_trajectory,
-                                     np.sin(
-                                         np.linspace(180 * math.pi / 180, 359.999 * math.pi / 180, 191)) * 0.525 + 0.2)
+
+            # Read csv file
+            coordinates = np.loadtxt(
+                open("uvispace/uvigui/tools/reinforcement_trainer/resources/testing_ackerman.csv", "r"),
+                delimiter=";")
+            x_trajectory = []
+            y_trajectory = []
+            for point in coordinates:
+                x_trajectory.append(point[0])
+                y_trajectory.append(point[1])
 
             self.ts.testing(load_name=self.hdf5_file_name, x_trajectory=x_trajectory, y_trajectory=y_trajectory, closed=False, differential_car=False)
 
