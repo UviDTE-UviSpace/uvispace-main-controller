80f79974dff08237d2a69790a9a06329749904ef
Javier Grandio <javiergrandiogonzalez@gmail.com>
2019-05-28 18:14:02 +0200

    csv_generator moved, new rewards

--- uvispace/uvinavigator/resources/trajectories/csvgenerator.py
+++ uvispace/uvinavigator/resources/trajectories/csvgenerator.py
@@ -26,22 +26,19 @@ x_trajectory = np.append(x_trajectory,
 y_trajectory = np.append(y_trajectory,
                          np.sin(np.linspace(270 * np.pi / 180, 180 * np.pi / 180, 81)) * 0.2 + 0.2)
 
-x_trajectory = np.linspace(0.7, -0.5, 240)
-y_trajectory = np.linspace(0.3, 1.1, 240)
-x_trajectory = np.append(np.linspace(0.7, -0.5, 280),
-                                     np.cos(np.linspace(90.01 * np.pi / 180, 269.99 * np.pi / 180, 180)) * 0.4 - 0.5)
-y_trajectory = np.append(np.linspace(0.3, 1.1, 280),
-                         np.sin(np.linspace(90.01 * np.pi / 180, 269.99 * np.pi / 180, 180)) * 0.4 + 0.7)
-x_trajectory = np.append(x_trajectory, np.linspace(-0.5, 0.7, 280))
-y_trajectory = np.append(y_trajectory, np.linspace(0.3, 1.1, 280))
-x_trajectory = np.append(x_trajectory,np.cos(np.linspace(90 * np.pi / 180, - 90 * np.pi / 180, 180)) * 0.4 + 0.7)
-y_trajectory = np.append(y_trajectory, np.sin(np.linspace(90 * np.pi / 180, - 90 * np.pi / 180, 180)) * 0.4 + 0.7)
+
+x_trajectory = np.append(np.cos(np.linspace(90 * np.pi / 180, 0.01 * np.pi / 180, 90)) * 0.4 - 1.5,
+                                     np.cos(np.linspace(180 * np.pi / 180, 539.99 * np.pi / 180, 360)) * 0.4 - 0.7)
+y_trajectory = np.append(np.sin(np.linspace(90 * np.pi / 180,0.01 * np.pi / 180, 90)) * 0.4 + 0.6,
+                         np.sin(np.linspace(180 * np.pi / 180, 539.99 * np.pi / 180, 360)) * 0.4 + 0.6)
+x_trajectory = np.append(x_trajectory,np.cos(np.linspace(360 * np.pi / 180, 300 * np.pi / 180, 90)) * 0.4 -1.5)
+y_trajectory = np.append(y_trajectory, np.sin(np.linspace(360 * np.pi / 180, 300 * np.pi / 180, 90)) * 0.4 + 0.6)
 
 
 
 l=[x_trajectory,y_trajectory]
 datos = np.asarray(l)
-np.savetxt("cdifferential_2_cameras.csv",   # Archivo de salida
+np.savetxt("verification.csv",   # Archivo de salida
            datos.T,        # Trasponemos los datos
            fmt="%f",       # Usamos números enteros
            delimiter=";")  # Para que sea un CSV de verdad
